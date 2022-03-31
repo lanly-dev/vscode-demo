f1d4033b309fd1dedd93451b19384f6f832cf1b1
lanly_dev <lanly-dev@users.noreply.github.com>
2022-03-30 18:25:58 -0700

    wip: context menu

--- src/extension.ts
+++ src/extension.ts
@@ -6,24 +6,25 @@ import CaProvider from './provider/codeAction'
 import ClProvider from './provider/codeLens'
 import IcProvider from './provider/inlineCompletion'
 
-let buttons: Buttons | null = null
+let buttons = new Buttons()
 
 export function activate(context: ExtensionContext) {
-  buttons = new Buttons()
   const { providedCodeActionKinds } = CaProvider
   const clp = new ClProvider()
   Ai.init()
   const rc = commands.registerCommand
   context.subscriptions.concat([
+    rc('sidekick.helper', () => helper()),
+    rc('sidekick.oneLine', () => oneLine()),
+    rc('sidekick.refactor', () => refactor()),
+  
     rc('sidekick.prediction.enable', () => enableFeature('enablePrediction')),
-    rc('sidekick.setting', () => Utility.openSetting('sidekick')),
-    rc('sidekick.oneLine', () => Ai.oneLine('helloworld')),
     rc('sidekick.refactor.cancel', () => clp.cancelRefactor()),
-    rc('sidekick.refactor.next', () => Ai.oneLine('helloworld')),
-    rc('sidekick.refactor.previous', () => Ai.oneLine('helloworld')),
-    rc('sidekick.refactor.start', (range, text) => clp.startRefactor(range, text)),
+    rc('sidekick.refactor.next', () => clp.nextRefactor()),
+    rc('sidekick.refactor.previous', () => clp.prevRefactor()),
     rc('sidekick.refactor.select', (range) => clp.select(range)),
-    rc('sidekick.refactor', () => Ai.oneLine('helloworld')),
+    rc('sidekick.refactor.start', (range, text) => clp.startRefactor(range, text)),
+    rc('sidekick.setting', () => Utility.openSetting('sidekick')),
     languages.registerCodeActionsProvider('*', new CaProvider, { providedCodeActionKinds }),
     languages.registerCodeLensProvider('*', clp),
     languages.registerInlineCompletionItemProvider({ pattern: '**' }, new IcProvider())
@@ -35,6 +36,18 @@ async function enableFeature(setting: string) {
   const flag = cf.get(setting)
   await cf.update(setting, !flag)
   if (buttons) buttons.refresh()
+}
+
+function oneLine() {
+  throw new Error('Function not implemented.')
+}
+
+function refactor() {
+  throw new Error('Function not implemented.')
+}
+
+function helper() {
+  throw new Error('Function not implemented.')
 }
 
 export function deactivate() {}
--- src/utility.ts
+++ src/utility.ts
@@ -1,7 +1,18 @@
-import { commands } from 'vscode'
+import { commands, Range, window } from 'vscode'
 
 export default class Utility {
   static openSetting(setting: string) {
     commands.executeCommand('workbench.action.openSettings', setting)
+  }
+
+  static getTextSelection() {
+    const activeTe = window.activeTextEditor
+    const start = activeTe?.selection.start
+    const end = activeTe?.selection.end
+    if (!start || !end) return
+    return activeTe?.document.getText(new Range(start, end))
+  }
+
+  static insertTextRightAfter(text: String) {
   }
 }
